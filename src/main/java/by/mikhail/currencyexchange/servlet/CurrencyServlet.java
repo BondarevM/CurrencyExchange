5f5ed7ad641cf1f11f9c3d98210f7aeda774a2c5
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-03-06 23:56:25 +0700

    write GET request and JSON responce to it

--- src/main/java/by/mikhail/currencyexchange/servlet/CurrencyServlet.java
+++ src/main/java/by/mikhail/currencyexchange/servlet/CurrencyServlet.java
@@ -1,6 +1,8 @@
 package by.mikhail.currencyexchange.servlet;
 
+import by.mikhail.currencyexchange.dto.CurrencyDto;
 import by.mikhail.currencyexchange.service.CurrencyService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,26 +12,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @WebServlet("/currencies")
 public class CurrencyServlet extends HttpServlet {
     private final CurrencyService currencyService = CurrencyService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        resp.setContentType("application/json");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
         try (PrintWriter writer = resp.getWriter()) {
             writer.write("<h1>Список валют</h1>");
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<CurrencyDto> currencies = currencyService.findAll();
+            String currenciesJson = objectMapper.writeValueAsString(currencies);
+            writer.write(currenciesJson);
 
-            currencyService.findAll().forEach(currencyDto -> {
-                writer.write("""
-                        <li>
-                            ID: %s  Code: %s  FullName: %s  Sign: %s
-                        </li>
-                        """.formatted(currencyDto.getID(),currencyDto.getCode(),currencyDto.getFullName(),currencyDto.getSign()));
 
-            });
+//            currencyService.findAll().forEach(currencyDto -> {
+//                writer.write("""
+//                        <li>
+//                            ID: %s  Code: %s  FullName: %s  Sign: %s
+//                        </li>
+//                        """.formatted(currencyDto.getID(),currencyDto.getCode(),currencyDto.getFullName(),currencyDto.getSign()));
+//
+//            });
 
         }
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,5 +8,8 @@
 <h1><%= "Hello World!" %></h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+<div>
+<a href="/currencies">Currencies</a>
+</div>
 </body>
 </html>
